0ee54e191d32866a4024834e8ff1ffab24140a9b
123wzchi-crypto <123wzchi@gmail.com>
2026-01-23 18:20:57 +0800

    Delete code directory

--- code/main/sig.m
+++
@@ -1,8 +0,0 @@
-%读取信号
-longSignal = X048_DE_time; 
-
-startIndex = 20000; % 截取的起始索引
-lengthOfSegment = 10000; % 截取的长度
-
-% 截取信号段
-signalSegment = longSignal(startIndex:(startIndex + lengthOfSegment - 1));
